201580139d5feb94621ec4933874dfba59f82868
soorajsunil1409 <sooraj.namboothiry@gmail.com>
2021-04-06 12:26:29 +0530

    Game Logic Done

--- tictactoe.py
+++ tictactoe.py
@@ -1,11 +1,12 @@
 from tkinter import *
+from tkinter import messagebox
 
 board = [
     ["", "", ""],
     ["", "", ""],
     ["", "", ""]
 ]
-size = (600, 700)
+size = (723, 823)
 
 class Board:
     btn_stats = []
@@ -22,11 +23,10 @@ class Board:
                 a.place(x=self.size[0]/3*j, y=self.size[0]/3*i, height=self.size[0]/3, width=self.size[0]/3)
                 self.btn_stats.append(a)
 
-        print(self.btn_stats)
-
     def update_btns(self):
         for btn in self.btn_stats:
             btn.config(text="")
+        self.enable_all_btns()
 
     def init_bindings(self):
         for btn in self.btn_stats:
@@ -34,9 +34,12 @@ class Board:
 
     def write(self, e):
         btn = e.widget
-        if btn["state"] == NORMAL: btn.config(text=self.current)
-        btn["state"] = DISABLED
-        self.flip()
+        if btn["state"] == NORMAL: 
+            btn.config(text=self.current)
+            self.win.update()
+            btn["state"] = DISABLED
+            self.check_win()
+            self.flip()
 
     def flip(self):
         self.current = "X" if self.current == "O" else "O"
@@ -45,12 +48,41 @@ class Board:
         self.current = "X"
         self.update_btns()
 
+    def disable_all_btns(self):
+        for btn in self.btn_stats:
+            btn["state"] = DISABLED
+
+    def enable_all_btns(self):
+        for btn in self.btn_stats:
+            btn["state"] = NORMAL
+
+    def check_win(self):
+        btn = self.btn_stats
+        if (
+            (btn[0]["text"] == "X" and btn[1]["text"] == "X" and btn[2]["text"] == "X") or (btn[0]["text"] == "O" and btn[1]["text"] == "O" and btn[2]["text"] == "O") or
+            (btn[3]["text"] == "X" and btn[4]["text"] == "X" and btn[5]["text"] == "X") or (btn[3]["text"] == "O" and btn[4]["text"] == "O" and btn[5]["text"] == "O") or
+            (btn[6]["text"] == "X" and btn[7]["text"] == "X" and btn[8]["text"] == "X") or (btn[6]["text"] == "O" and btn[7]["text"] == "O" and btn[8]["text"] == "O") or
+
+            (btn[0]["text"] == "X" and btn[3]["text"] == "X" and btn[6]["text"] == "X") or (btn[0]["text"] == "O" and btn[3]["text"] == "O" and btn[6]["text"] == "O") or
+            (btn[1]["text"] == "X" and btn[4]["text"] == "X" and btn[7]["text"] == "X") or (btn[1]["text"] == "O" and btn[4]["text"] == "O" and btn[7]["text"] == "O") or
+            (btn[2]["text"] == "X" and btn[5]["text"] == "X" and btn[8]["text"] == "X") or (btn[2]["text"] == "O" and btn[5]["text"] == "O" and btn[8]["text"] == "O") or
+            
+            (btn[0]["text"] == "X" and btn[4]["text"] == "X" and btn[8]["text"] == "X") or (btn[0]["text"] == "O" and btn[4]["text"] == "O" and btn[8]["text"] == "O") or
+            (btn[2]["text"] == "X" and btn[4]["text"] == "X" and btn[6]["text"] == "X") or (btn[2]["text"] == "O" and btn[4]["text"] == "O" and btn[6]["text"] == "O")
+        ):
+            messagebox.showinfo("YAY!!!", f"{self.current} won the match")
+            self.disable_all_btns()
+        elif (btn[0]["text"] != "" and btn[1]["text"] != "" and btn[2]["text"] != "" and btn[3]["text"] != "" and btn[4]["text"] != "" and btn[5]["text"] != "" and btn[6]["text"] != "" and btn[7]["text"] != "" and btn[8]["text"] != ""):
+            messagebox.showinfo("TIE!!!", f"The match was a tie!!")
+            self.disable_all_btns()
+            
+
 
 def main():
     win = Tk()
     win.title("Tic - Tac - Toe")
     win.resizable(False, False)
-    win.geometry("600x700")
+    win.geometry(f"{size[0]}x{size[1]}")
 
     app = Board(win, size)
     app.create_board()
